925cdc1030b96f1835735742214577fb1f70da95
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-10-17 02:44:52 -0700

    Simple Formatting

--- src/index.ts
+++ src/index.ts
@@ -17,12 +17,12 @@ interface Instrument {
 
 var instruments: Record<string, Instrument> = {
   bass: {
-    keys: ["v","b"],
+    keys: ["v", "b"],
     start: 0.105,
     url: bass
   },
   snare: {
-    keys: ["n","m"],
+    keys: ["n", "m"],
     start: 0.201,
     volume: 0.85,
     url: snare
@@ -57,20 +57,21 @@ var instruments: Record<string, Instrument> = {
     url: bell
   }
 };
+
 Object.keys(instruments).forEach(key => {
   var instrument: Instrument = instruments[key]!;
   var audio = new Audio(instrument.url);
   audio.currentTime = (instrument.start !== undefined) ? instrument.start : 0;
   audio.volume = (instrument.volume !== undefined) ? instrument.volume : 1;
   instrument.audio = audio;
-  document.addEventListener("keydown",event => {
+  document.addEventListener("keydown", event => {
     if (!instrument.keys.includes(event.key)) return;
-    if (key == "hi_hat_open"){
+    if (key == "hi_hat_open") {
       var hi_hat_closed: Instrument = instruments["hi_hat_closed"]!;
       hi_hat_closed.audio!.pause();
       hi_hat_closed.audio!.currentTime = (hi_hat_closed.start !== undefined) ? hi_hat_closed.start : 0;
     }
-    if (key == "hi_hat_closed"){
+    if (key == "hi_hat_closed") {
       var hi_hat_open: Instrument = instruments["hi_hat_open"]!;
       hi_hat_open.audio!.pause();
       hi_hat_open.audio!.currentTime = (hi_hat_open.start !== undefined) ? hi_hat_open.start : 0;
