23dff47886f45be0baccd5bf7f40490e1db87972
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-10-20 12:35:09 -0700

    Gamepad State!
    Adding gamepad support for activating drum queues! Going to have to refactor the main script a bit, as it should essentially be moved over to the `App.tsx` component, now that things are build with SolidJS.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,31 @@
+import { createEffect, createSignal, onCleanup } from "solid-js";
+import { GamepadObserver } from "gamepad-state";
+
 export default function App() {
+  const [getGamepad, setGamepad] = createSignal<Gamepad | null>(null);
+
+  const observer = new GamepadObserver(record => {
+    if (!record.gamepad.mapping) return; // Also related to the Chrome bug below.
+
+    switch (record.type) {
+      case "connect":
+      case "input": return setGamepad(record.gamepad);
+      case "disconnect": return setGamepad(null);
+    }
+  });
+
+  // This is to account for a bug in Chrome macOS where my SteelSeries Nimbus shows up as two controllers.
+  observer.observe(0);
+  observer.observe(1);
+
+  createEffect(() => {
+    console.log(getGamepad());
+  });
+
+  onCleanup(() => {
+    observer.disconnect();
+  });
+
   return (
     <>
       <header>
